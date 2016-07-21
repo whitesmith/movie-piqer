6dc94dc01d4e86e74f5d09fca3e52c84c899dc90
Pedro <pjaneiro@student.dei.uc.pt>
2016-07-21 17:17:33 +0100

    More Heroku config files

--- moviepiqer/settings.py
+++ moviepiqer/settings.py
@@ -133,3 +133,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
